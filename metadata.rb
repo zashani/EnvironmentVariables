0fcc5bec50eef43ec4c07d1e60ec041c0633746f
zashani <z.ashani@agentvi.com>
2014-05-12 19:35:13 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,6 @@
 name 'EnvironmentVariables'
 depends 'phantomjs'
+
+
+recipe 'EnvironmentVariables::configure' , 'configure environment variables'
+recipe 'EnvironmentVariables::phantomjs' , 'install phatomjs'
